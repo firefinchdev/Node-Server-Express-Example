503731525ebb2a5a2c0ab8248d3d1cbcb0b4e65f
Anirudh Gupta <anirudhgupta281998@gmail.com>
2018-01-27 19:45:58 +0530

    Express Middleware

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
+
+//Express Middleware Basic (Static)
 app.use(express.static(__dirname + '/public')); //can use localhost:3000/help.html
+
+//Express Middleware
+app.use((req, res, next) => {
+    var curTime = new Date().toString();
+    var logLine = `${curTime}: ${req.method} ${req.url}`;
+
+    //Log to console
+    console.log(logLine);
+
+    //Log to File server.log
+    fs.appendFile('server.log', logLine + '\n', () => {
+        //Callback function
+    });
+
+    //Important to call next() after end of Middleware to resume
+    next();
+
+});
+
+//When site is under Maintenance, uncomment this code - NOTE: It does not use next()
+/*app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});*/
 
 hbs.registerHelper('currentYear', ()=> {
     return new Date().getFullYear();
